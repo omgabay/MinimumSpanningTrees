7c2b65cb42797a4ef95527c94aeb07811e7395cf
omerg <omer.gabay@yahoo.com>
2022-04-19 14:24:15 +0200

    MST-search using Kruskal's Algorithm

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/omerg/CLionProjects/spanning-trees
+# Install script for directory: C:/Users/omerg/CLionProjects/MinimumSpanningTrees
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/omerg/CLionProjects/spanning-trees/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/omerg/CLionProjects/MinimumSpanningTrees/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
